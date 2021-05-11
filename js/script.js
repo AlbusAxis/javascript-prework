bdba79d9819989d629381d01b7e4030d75ae7f66
Michal Kulczycki <kulczycki.mj@gmail.com>
2021-05-11 15:43:23 +0200

    kod gry

--- js/script.js
+++ js/script.js
@@ -21,9 +21,9 @@ function getMoveName(argMoveId) {
   if (argMoveId == 1) {
     return 'kamień';
   } else if (argMoveId == 2) {
-    return 'papier ';
+    return 'papier';
   } else if (argMoveId == 3) {
-    return 'nożyce ';
+    return 'nożyce';
   } else {
     printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
     return 'kamień';
@@ -37,17 +37,17 @@ function displayResult(argPlayerMove, argComputerMove) {
   console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
   if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
     printMessage('Wygrywasz!');
-  } else if (argPlayerMove == 'kamień ' && argComputerMove == 'nożyce ') {
-  } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier ') {
+  } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+    printMessage('Wygrywasz!');
+  } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
     printMessage('Wygrywasz!');
   } else if (argPlayerMove == argComputerMove) {
-    printMessage('Remis ');
+    printMessage('remis!');
   } else {
     printMessage('Przegrywasz :(');
   }
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
-printMessage('Wygrywasz!');
 playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('wybór ruchu gracza to: ' + playerInput);
 playerMove = getMoveName(playerInput);
