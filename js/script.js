3332d981a11c34d30ddedf2ccb265232c61a5aed
Michal Kulczycki <kulczycki.mj@gmail.com>
2021-05-11 10:57:42 +0200

    papier kamien nozyce - kod

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + 'Wygrałem ');
+
+
+
+// Papier kamień nożyce 
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier ';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
